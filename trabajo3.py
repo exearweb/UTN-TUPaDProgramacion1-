f23484d87f2620d334dc0bfa2e60c8b475872e03
Exequiel Arancibia Ruiz <exear11@gmail.com>
2025-08-29 20:31:37 -0300

    Trabajo Practico N3

---
+++ trabajo3.py
@@ -0,0 +1,21 @@
+#Ejercicio 1
+edad=int(input ("ingrese su edad"))
+if(edad>18):
+    print("Es mayor de edad")
+#Ejercicio 2
+nota= int(input ("ingrese su nota"))
+if(nota>6):
+    print("Aprobado")
+elif(nota<6):
+    print("Desaprobado")
+#Ejercicio 3
+numero=int(input("Ingrese un numero"))
+if(numero % 2 ==0):
+    print("numero par")
+else:
+    print("es impar")
+print("hola")
+numero1=int(input("ingrese un numero"))
+if(numero1 % 2 !=0):
+    print("es impar")
+    #Ejercicio 4
